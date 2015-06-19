c39abf7c9aab91ab7bd0cd017ca6d2edf923b28e
Eron Castro <eroncastro@gmail.com>
2015-06-18 21:24:18 -0300

    Updated Rails and related gems

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615232900) do
+ActiveRecord::Schema.define(version: 20150618212000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,9 +39,10 @@ ActiveRecord::Schema.define(version: 20150615232900) do
     t.string   "name"
     t.string   "email"
     t.string   "sector"
-    t.string   "status"
+    t.string   "massage_status"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status"
   end
 
 end
